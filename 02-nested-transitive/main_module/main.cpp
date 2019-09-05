10813530be6f251a791d27152a3e5161f326658d
Kevin Winata <kevinwinatamichael@gmail.com>
2019-09-05 16:04:25 +0800

    make sub-sub-module to be private, i.e. can't be access from outside

--- 02-nested-transitive/main_module/main.cpp
+++ 02-nested-transitive/main_module/main.cpp
@@ -1,6 +1,9 @@
 #include "subtract/subtract.h"
 #include "multiply.h"
-#include "add.h"
+
+// Will result error because of PRIVATE
+// #include "add.h"
+
 #include<stdio.h>
 
 
@@ -8,5 +11,6 @@ int main(int argc, char *argv[])
 {
     printf("2-1=%i\n", subtract(2, 1));
     printf("3*4=%i\n", multiply(3, 4));
+    // printf("%i", add(4, 9));
     return 0;
 }
