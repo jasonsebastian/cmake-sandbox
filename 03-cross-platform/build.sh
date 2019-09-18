d7f3b37b391016b96f477928e7a1124d741a5b85
Jason Sebastian <jason.sebastian11@gmail.com>
2019-09-18 14:52:37 +0800

    Remove BUILD_FOR_* options

--- 03-cross-platform/build.sh
+++ 03-cross-platform/build.sh
@@ -7,10 +7,9 @@ build_for_android() {
     for arch in armeabi-v7a arm64-v8a x86 x86_64; do
         mkdir $arch
         pushd $arch
-        cmake -DCMAKE_SYSTEM_NAME=Android \
+        cmake ../.. -DCMAKE_SYSTEM_NAME=Android \
             -DCMAKE_ANDROID_ARCH_ABI=$arch \
-            -DCMAKE_ANDROID_STL_TYPE=c++_static \
-            -DBUILD_FOR_ANDROID=ON ../..
+            -DCMAKE_ANDROID_STL_TYPE=c++_static
         cmake --build .
         popd
     done
@@ -18,10 +17,8 @@ build_for_android() {
 
 build_for_macos() {
     mkdir build && cd build
-    cmake -DCMAKE_C_COMPILER=clang \
-        -DCMAKE_CXX_COMPILER=clang++ \
-        -DBUILD_FOR_MACOS=ON ..
+    cmake .. -DCMAKE_SYSTEM_NAME=Darwin
     cmake --build .
 }
 
-build_for_macos
+build_for_android
