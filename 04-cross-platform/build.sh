295b12dda428deb5f1870e7b6e2fd2998822a8db
Jason Sebastian <jason.sebastian11@gmail.com>
2019-09-19 11:28:00 +0800

    Refactor clean macro

--- 04-cross-platform/build.sh
+++ 04-cross-platform/build.sh
@@ -1,10 +1,12 @@
-if [ -d "build" ]; then
-    rm -rf build
-fi
-
-mkdir build && cd build
+clean() {
+    if [ -d "build" ]; then
+        rm -rf build
+    fi
+}
 
 build_for_android() {
+    clean
+    mkdir build && cd build
     for arch in armeabi-v7a arm64-v8a x86 x86_64; do
         mkdir $arch
         pushd $arch
@@ -17,11 +19,15 @@ build_for_android() {
 }
 
 build_for_macos() {
+    clean
+    mkdir build && cd build
     cmake .. -DCMAKE_SYSTEM_NAME=Darwin
     cmake --build .
 }
 
 build_for_linux() {
+    clean
+    mkdir build && cd build
     cmake .. -DCMAKE_SYSTEM_NAME=Linux \
         -DCMAKE_SYSTEM_PROCESSOR=arm
     cmake --build .
@@ -31,16 +37,17 @@ if [ $# -eq 0 ]; then
     echo "Usage: ./build.sh <compile_target>"
 else
     case $1 in
-        android)
-            build_for_android
-            ;;
-        macos)
-            build_for_macos
-            ;;
-        linux)
-            build_for_linux
-            ;;
-        *)
-            echo "Compile target not recognized"
+    android)
+        build_for_android
+        ;;
+    macos)
+        build_for_macos
+        ;;
+    linux)
+        build_for_linux
+        ;;
+    *)
+        echo "Compile target not recognized"
+        ;;
     esac
 fi
