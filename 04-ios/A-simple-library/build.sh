94712daa8f020ce74ea2eb63ae64902a1d9f0a51
Jason Sebastian <jason.sebastian11@gmail.com>
2019-09-24 16:16:05 +0800

    Link iOS against static library

--- 04-ios/A-simple-library/build.sh
+++ 04-ios/A-simple-library/build.sh
@@ -2,13 +2,23 @@ clean() {
     if [ -d "build" ]; then
         rm -rf build
     fi
-    mkdir build && cd build
+    mkdir build
+    if [ -d "distribution" ]; then
+        rm -rf distribution
+    fi
+    mkdir distribution
 }
 
 build_for_ios() {
     clean
+    cd build
+    PLATFORM="iPhoneSimulator"
     cmake .. -GXcode \
-        -DCMAKE_SYSTEM_NAME=iOS
+        -DCMAKE_SYSTEM_NAME=iOS \
+        -DCMAKE_OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/${PLATFORM}.platform/Developer/SDKs/${PLATFORM}.sdk" \
+        "-DCMAKE_OSX_ARCHITECTURES=armv7;arm64;x86_64" \
+        -DCMAKE_OSX_DEPLOYMENT_TARGET=11.4 \
+        -DCMAKE_IOS_INSTALL_COMBINED=YES
     cmake --build .
 }
 
