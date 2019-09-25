f228567422ff203af593815db319f5846d57a905
Jason Sebastian <jason.sebastian11@gmail.com>
2019-09-25 16:36:35 +0800

    Android starter code

---
+++ 06-common-core/android/app/src/main/java/com/example/android/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView addTextView = findViewById(R.id.add);
+        addTextView.setText("add(1, 2) = " + add(1, 2));
+
+        TextView subtractTextView = findViewById(R.id.subtract);
+        subtractTextView.setText("subtract(3, 2) = " + subtract(3, 2));
+    }
+
+    public native int add(int a, int b);
+
+    public native int subtract(int a, int b);
+}
