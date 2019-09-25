d4dbd5cfed1bf31a0bf63be778f821d25fb484e0
Jason Sebastian <jason.sebastian11@gmail.com>
2019-09-25 11:38:04 +0800

    iOS link against shared library

---
+++ 04-ios/B-shared-library/examples/ios/ios/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  ios
+//
+//  Created by Jason Sebastian on 9/20/19.
+//  Copyright © 2019 Jason Sebastian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
+@end
+
