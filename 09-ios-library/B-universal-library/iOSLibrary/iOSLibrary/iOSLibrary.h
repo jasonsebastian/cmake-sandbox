4e6514610a28f25da4475838627ba08b0694f61c
Jason Sebastian <jason.sebastian11@gmail.com>
2019-10-01 14:46:40 +0800

    Starter code for universal library

---
+++ 09-ios-library/B-universal-library/iOSLibrary/iOSLibrary/iOSLibrary.h
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+
+@interface iOSLibrary : NSObject
+- (int)add:(int)a with:(int)b;
+@end
---
+++ 09-ios-library/B-universal-library/iOSLibrary/iOSLibrary/iOSLibrary.m
@@ -0,0 +1,9 @@
+#import "iOSLibrary.h"
+
+@implementation iOSLibrary
+
+- (int)add:(int)a with:(int)b {
+    return a + b;
+}
+
+@end
