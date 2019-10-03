87e6434b54f1c7c4b60a0605c9c867fab0905b50
Jason Sebastian <jason.sebastian11@gmail.com>
2019-10-03 14:39:45 +0800

    Remove unnecessary code

--- 08-android-library/library/native/conanfile.py
+++ 08-android-library/library/native/conanfile.py
@@ -6,7 +6,7 @@ class CalcConan(ConanFile):
     version = "0.1.3"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
-    default_options = "shared=False"
+    default_options = "shared=True"
     generators = "cmake"
     exports_sources = "calc/*"
     android_abi = None
@@ -27,22 +27,15 @@ class CalcConan(ConanFile):
             cmake.definitions["ANDROID_ABI"] = self.android_abi
             cmake.definitions["CMAKE_ANDROID_ARCH_ABI"] = self.android_abi
             cmake.definitions["CMAKE_ANDROID_NDK"] = os.getenv('ANDROID_NDK_HOME')
-        elif self.settings.os == 'Linux':
-            cmake.definitions["CMAKE_SYSTEM_PROCESSOR"] = "arm"
-        elif self.settings.os != 'Macos':
-            raise ValueError("Currently we only support build for Android, Linux, and Macos")
 
         cmake.configure(source_folder="calc")
         cmake.build()
 
     def package(self):
-        lib_path = os.path.join(str(self.settings.os), str(self.android_abi))
-        self.copy("*.h", dst="include", keep_path=False)
-        self.copy("*.lib", dst=lib_path, keep_path=False)
-        self.copy("*.dll", dst=lib_path, keep_path=False)
-        self.copy("*.so", dst=lib_path, keep_path=False)
-        self.copy("*.dylib", dst=lib_path, keep_path=False)
-        self.copy("*.a", dst=lib_path, keep_path=False)
+        if self.settings.os == 'Android':
+            lib_path = os.path.join(str(self.settings.os), str(self.android_abi))
+            self.copy("*.h", dst="include", keep_path=False)
+            self.copy("*.so", dst=lib_path, keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["calc"]
